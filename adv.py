e48334a9ea9e61a2ce50fd443c213744df3c02ff
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-03-19 14:22:54 +0000

    first pass solution all tests passing

--- adv.py
+++ adv.py
@@ -12,11 +12,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
+# map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -29,22 +29,39 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
+route = []
 
 q = Queue()
-q.enqueue((traversal_path, player.current_room))
+q.enqueue((route, player.current_room))
 visited = set()
+routes = list()
 
 while q.size() > 0:
-    (traversal_path, current_room) = q.dequeue()
-    if len(traversal_path) == len(world.rooms):
-        break
+    (route, current_room) = q.dequeue()
+    next_rooms = [getattr(current_room, f'{exit}_to') for exit in ['n', 's', 'e', 'w']]
+    unvisited_next_rooms = [room.id for room in next_rooms if room is not None and room.id not in visited]
+    if len(unvisited_next_rooms) == 0:
+        routes.append(route)
     else:
         for exit in current_room.get_exits():
             visited.add(current_room.id)
             next_room = getattr(current_room, f'{exit}_to')
             if next_room.id not in visited:
-                q.enqueue(([*traversal_path, exit], next_room))
+                q.enqueue(([*route, exit], next_room))
+
+routes.sort(key=len)
+
+traversal_path = []
+substitutes = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+
+for i in range(len(routes)):
+    if i < len(routes) - 1:
+        forward = list(routes[i])
+        backward = [substitutes[direction] for direction in forward[::-1]]
+        traversal_path.extend(forward)
+        traversal_path.extend(backward)
+    else:
+        traversal_path.extend(routes[i])
 
 # TRAVERSAL TEST
 visited_rooms = set()
